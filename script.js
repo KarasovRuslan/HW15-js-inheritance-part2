cff644abbeb8c28c1f396bdc8595fe88c6939a25
KarasovRuslan <karasov.ruslan@icloud.com>
2021-12-26 05:36:08 +0200

    HW15 fix

--- script.js
+++ script.js
@@ -1,45 +1,60 @@
+let clockTime = document.getElementById('clockTime');
+let clockTime2 = document.getElementById('clockTime2');
+let clockTime3 = document.getElementById('clockTime3');
+
 class Clock {
-    constructor() { 
-        this.clockTime = document.getElementById('clockTime');
+    constructor(e) { 
+        this.e = e;
+        this.isfullFormat = true;
+        this.e.addEventListener('click', () => this.setTimeFormat());
     };
+
     getTime() {
-        return new Date();
+        let date = new Date();
+        let hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();;
+        let minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+        let seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+        
+        return   this.isfullFormat ? `${hours}:${minutes}:${seconds}` : `${hours}:${minutes}`;
+    };
+
+    render() {
+        this.e.innerText = this.getTime(); 
     };
 
     setTimeFormat() {  
-        if (this.clockTime.classList.contains('fullFormat')) {   
-            clockFullFormat.render();  
-        } else {
-            clockShortFormat.render(); 
-        };
+        this.isfullFormat = !this.isfullFormat;
     };
 };
 
-this.clockTime.addEventListener('click', () => {
-    this.clockTime.classList.toggle('fullFormat');
-});
-
-let clock = new Clock();
-
-class СlockFullFormat extends Clock {
-    render() {
-        this.hours = this.getTime().getHours() < 10 ? '0' + this.getTime().getHours() : this.getTime().getHours();           
-        this.minutes = this.getTime().getMinutes() < 10 ? '0' + this.getTime().getMinutes() : this.getTime().getMinutes();  
-        this.seconds = this.getTime().getSeconds() < 10 ? '0' + this.getTime().getSeconds() : this.getTime().getSeconds();  
-
-        this.clockTime.innerText = this.hours + ":" + this.minutes + ":" + this.seconds;          
+class FirstClock extends Clock {
+    getTime() {
+        let date = new Date();
+        let hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();;
+        let minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+        let seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+        let milliSec = date.getMilliseconds();
+        
+        return   this.isfullFormat ? `${hours}:${minutes}:${seconds}:${milliSec}` : `${hours}:${minutes}`;
     };
 };
-class СlockShortFormat extends Clock {
-    render() {
-        this.hours = this.getTime().getHours() < 10 ? '0' + this.getTime().getHours() : this.getTime().getHours();           
-        this.minutes = this.getTime().getMinutes() < 10 ? '0' + this.getTime().getMinutes() : this.getTime().getMinutes();          
-
-        this.clockTime.innerText = this.hours + ":" + this.minutes;      
+class SecondClock extends Clock {
+    getTime() {
+        let date = new Date();
+        let hours = date.getHours() < 10 ? '0' + date.getHours() : date.getHours();;
+        let minutes = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+        let seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+        let milliSec = date.getMilliseconds();
+        
+        return   this.isfullFormat ? `${hours}:${minutes}:${seconds}:${milliSec}` : `${hours}:${minutes}:${seconds}`;
     };
 };
 
-let clockFullFormat = new СlockFullFormat();
-let clockShortFormat = new СlockShortFormat();
+let clock1 = new Clock(clockTime);
+setInterval(() => clock1.render(), 250);
 
-setInterval(() => clock.setTimeFormat(), 250);
+let clock2 = new FirstClock(clockTime2);
+setInterval(() => clock2.render(), 250);
+
+let clock3 = new SecondClock(clockTime3);
+setInterval(() => clock3.render(), 250); 
